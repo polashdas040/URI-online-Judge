7286a62bf61bc7ff083efbcdab9ef1879f0e4c82
Polash Chandra Das <78200329+polashdas040@users.noreply.github.com>
2021-05-11 00:50:57 +0600

    Add files via upload

---
+++ URI_Online_Judge_1065.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+	
+	int i, a, count = 0;
+	
+	for (i = 0; i<5; i++){
+		scanf("%d",&a);
+		
+		if(a%2==0){
+			count++;
+		}
+	}
+	
+	printf("%d valores pares\n",count);
+	
+	return 0;
+}

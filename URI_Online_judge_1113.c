f842bfcf5a47a6004997aab3e27bdf9fd454b1ee
Polash Chandra Das <78200329+polashdas040@users.noreply.github.com>
2021-06-01 20:56:15 +0600

    Add files via upload

---
+++ URI_Online_judge_1113.c
@@ -0,0 +1,16 @@
+#include <stdio.h> 
+int main() {
+	
+	int x,y;
+	
+	while(scanf("%d%d",&x,&y) && x != y){
+		
+		if(x<y){
+			printf("Crescente\n");
+		}else{
+			printf("Decrescente\n");
+		}
+	}
+ 				   
+    return 0;
+}

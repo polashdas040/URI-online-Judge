59d34f3cad0717380a4d509d2b0b6947e68dda06
Polash Chandra Das <78200329+polashdas040@users.noreply.github.com>
2021-07-03 16:20:25 +0600

    Add files via upload

---
+++ URI_Online_judge_1145.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+	int X,Y,i;
+
+	scanf("%d%d",&X,&Y);
+
+	for(i=1; i<=Y; i++) {
+		if(i %X==0) {
+			printf("%d\n",i);
+		} else {
+			printf("%d ",i);
+		}
+
+	}
+
+	return 0;
+}
+
